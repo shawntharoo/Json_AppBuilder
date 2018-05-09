d4ca5f832b8b1e489cef076da21f7b8feecf2541
sandakelum <tharooshawn@gmail.com>
2018-05-09 12:16:20 +0530

    session

--- server/model/user_model.js
+++ server/model/user_model.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
     username: {type: String, required: true, unique: true},
     email: {type: String, required: true},
-    
     authyId: String,
     hashed_password: String
 });
